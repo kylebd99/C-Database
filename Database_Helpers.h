40e96d5a84e7332270341b000f4893635ece4123
kylebd99 <kylebd99@gmail.com>
2017-11-29 19:33:36 -0500

    Val Update

---
+++ Database_Helpers.h
@@ -0,0 +1,7 @@
+#include "Database_Class.h"
+
+//TODO Function which reads in a csv and creates a Database
+
+//TODO Function which calculates mean data for each numeric column
+
+//TODO Function which does linear regression
